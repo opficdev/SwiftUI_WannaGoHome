0d544f87c5b5613267c960362374955b4293d305
opfic <opficdev@gmail.com>
2024-12-05 18:28:18 +0900

    2024/12/05 DDayView 추가

---
+++ WannaGoHome/View/DDayView.swift
@@ -0,0 +1,20 @@
+//
+//  DDayView.swift
+//  WannaGoHome
+//
+//  Created by opfic on 12/5/24.
+//
+
+import SwiftUI
+
+struct DDayView: View {
+    var body: some View {
+        VStack {
+            
+        }
+    }
+}
+
+#Preview {
+    DDayView()
+}
