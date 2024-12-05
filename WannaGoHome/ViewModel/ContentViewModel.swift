d0c15aa7409d50d205dd0bde6dc0ec64797826a2
opfic <opficdev@gmail.com>
2024-12-05 18:27:42 +0900

    2024/12/05 ContentViewModel 추가

---
+++ WannaGoHome/ViewModel/ContentViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  ContentVM.swift
+//  WannaGoHome
+//
+//  Created by opfic on 12/5/24.
+//
+
+import Foundation
+
+class ContentViewModel: ObservableObject {
+    @Published var selectedTab = "DDayView"
+}
