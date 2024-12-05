b62b4abe113c00748bb674c9cc29282a438b91fd
opfic <opficdev@gmail.com>
2024-12-05 17:32:06 +0900

    2024/12/5 최초 커밋

---
+++ WannaGoHome/WannaGoHomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  WannaGoHomeApp.swift
+//  WannaGoHome
+//
+//  Created by opfic on 12/5/24.
+//
+
+import SwiftUI
+
+@main
+struct WannaGoHomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
