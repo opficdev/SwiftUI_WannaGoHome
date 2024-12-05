ef234f53eaa95c2dcdb24beb27f0fdec8876ff58
opfic <opficdev@gmail.com>
2024-12-05 18:28:39 +0900

    2024/12/05 LoginView 추가

---
+++ WannaGoHome/View/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  WannaGoHome
+//
+//  Created by opfic on 12/5/24.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView()
+}
