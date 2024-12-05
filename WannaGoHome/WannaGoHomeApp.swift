f72bf9b2d1a6d30f21eb46a706cbbcc4daadaefa
opfic <opficdev@gmail.com>
2024-12-05 17:40:14 +0900

    2024/12/05 파일 추가 및 분류

--- WannaGoHome/WannaGoHomeApp.swift
+++ WannaGoHome/WannaGoHomeApp.swift
@@ -9,6 +9,9 @@ import SwiftUI
 
 @main
 struct WannaGoHomeApp: App {
+    init() {
+        UIView.appearance().overrideUserInterfaceStyle = .light
+    }
     var body: some Scene {
         WindowGroup {
             ContentView()
